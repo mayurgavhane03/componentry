e2b6e910cf3a5f1a254b526e6fc488dc9f07e157
Mayur Gavhane <mayurrgavhane2003@gmail.com>
2026-06-13 01:15:55 +0530

    react button example is udated

--- apps/examples-react/src/main.tsx
+++ apps/examples-react/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
  import App from './App';
-
+import "@componentry/theme/styles/light.css"
+import "@componentry/theme/styles/dark.css"
  
 createRoot(document.getElementById('root')!).render(<App />);
